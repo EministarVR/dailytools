7ab0bb75c2a94f36b3fd2ad6502d6cf497d28dd1
EministarVR <benounnaelemin@gmail.com>
2024-10-04 18:05:22 +0200

    Delete image-upload.js

--- image-upload.js
+++
@@ -1,5 +0,0 @@
-// Dies kann auch in script.js integriert werden, aber hier getrennt, um die Struktur zu verbessern
-document.addEventListener('DOMContentLoaded', () => {
-    const imageUpload = document.getElementById('image-upload');
-    imageUpload.addEventListener('change', insertImage);
-});
